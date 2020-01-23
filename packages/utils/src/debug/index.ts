8d18950aea1b90e1f4ca2a96a80ed48cd97949a1
Arturs Vonda <arturs@artursvonda.lv>
2020-01-24 00:34:34 +0200

    Add few debug functions to utils

--- packages/utils/src/debug/index.ts
+++ packages/utils/src/debug/index.ts
@@ -1,9 +1,20 @@
 import chalk from 'chalk';
 
 const infoText = chalk;
+const logText = chalk;
+const errorText = chalk.bgRed.white;
 
 type Input = string | { toString(): string };
 
 export const info = (text: TemplateStringsArray | Input, ...args: Input[]) => {
     console.log(infoText(text as string, ...(args as string[])));
+};
+
+export const log = (text: TemplateStringsArray | Input, ...args: Input[]) => {
+    console.log(logText(text as string, ...(args as string[])));
+};
+
+export const error = (text: TemplateStringsArray | Input, ...args: Input[]) => {
+    console.error(errorText('!Error!'));
+    console.error(logText(text as string, ...(args as string[])));
 };
