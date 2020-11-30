390f42935de3b2ffb55b513427a7c69bbff266ae
Arturs Vonda <arturs@artursvonda.lv>
2020-11-30 15:46:57 +0200

    Update prettier config

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,11 +1,5 @@
 module.exports = {
-    printWidth: 100,
-    trailingComma: 'all',
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
-    quoteProps: 'as-needed',
-    bracketSpacing: true,
-    jsxBracketSameLine: false,
-    arrowParens: 'avoid',
+  printWidth: 100,
+  trailingComma: 'all',
+  singleQuote: true,
 };
